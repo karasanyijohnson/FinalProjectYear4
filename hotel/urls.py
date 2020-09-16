94a4cd7c69e59640f66db016a494dcf5df24a0db
johnsonkarasanyi <johnsonkarasanyi@gmail.com>
2020-09-16 13:09:31 +0200

    Added allauth and managed booking_list permissions

---
+++ hotel/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import RoomListView, BookingList, BookingView, RoomDetailView
+
+app_name = 'hotel'
+
+urlpatterns = [
+    path('room_list/', RoomListView, name='RoomList'),
+    path('booking_list/', BookingList.as_view(), name='BookingList'),
+    path('book/', BookingView.as_view(), name='BookingView'),
+    path('room/<category>', RoomDetailView.as_view(), name='RoomDetailView')
+]
